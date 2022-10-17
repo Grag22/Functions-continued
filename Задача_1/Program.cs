f8c9d90eb6ac2d1948183d068af12857059f1e0e
Кушнаренко Григорий <Gr4n777.yandex.ru>
2022-10-17 11:57:04 +0300

    Домашнее задание 4

---
+++ Задача_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+for (int i = 1; i < b; i++)
+    result = result * a;
+Console.WriteLine(a + " в степени " + b + " = " + result);
---
+++ Задача_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+string number = ReadLine("Введите число: ");
+Console.WriteLine(GetSum(number));
+
+string ReadLine(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine()!;
+}
+
+int GetSum(string number)
+{
+    int sum = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        sum += Convert.ToInt32(number[i]) - 48;
+
+    }
+    return sum;
+}
